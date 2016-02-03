e626b6a3e233b60a0874e96fa04fe13bc130828b
Kuipers,R. (Robin) <r.kuipers@students.uu.nl>
2016-02-03 18:49:16 +0100

    Implemented Distributed Server
    Signed-off-by: Kuipers,R. (Robin) <r.kuipers@students.uu.nl>

--- timekeeper/src/Network/TimeKeeper/Protocol.hs
+++ timekeeper/src/Network/TimeKeeper/Protocol.hs
@@ -4,6 +4,9 @@ import Data.Binary
 import Data.Typeable
 import GHC.Generics
 
+import Control.Distributed.Process
+import System.IO
+
 -- | Definition of a path.
 --
 -- A path consists of zero or more node names.
@@ -19,17 +22,26 @@ instance Binary Path
 type NodeName = Text
 
 -- | The set of commands emitted by clients.
-data Action = Put         { path :: Path, putNewValue :: Maybe Text }
-            | Get         { path :: Path }
-            | GetChildren { path :: Path }
-            | Subscribe   { path :: Path }
-            | Unsubscribe { path :: Path }
+data Action = Put          { path :: Path, putNewValue :: Maybe Text }
+            | Get          { path :: Path }
+            | GetChildren  { path :: Path }
+            | Subscribe    { path :: Path }
+            | Unsubscribe  { path :: Path }
+            | NotifyLeader { name :: Text }
             deriving (Eq, Ord, Show, Read, Typeable, Generic)
 instance Binary Action
 
--- | The set of events emitted by servers.
+-- | The set of events emitted by servers to clients.
 data Event = Updated     { eventPath :: Path, oldValue :: Maybe Text, newValue :: Maybe Text }
            | ValueIs     { eventPath :: Path,                         newValue :: Maybe Text }
            | ChildrenAre { eventPath :: Path, children :: [Text] }
            deriving (Eq, Ord, Show, Read, Typeable, Generic)
 instance Binary Event
+
+-- | The set of updates emitted by servers to each other
+data Update = PutUpdate    { pid :: ProcessId, updatePath :: Path, updatedValue :: Maybe Text }
+            | NewClient    { pid :: ProcessId, clientName :: Text }
+        --  | Subscribe   
+        --  | Unsubscribe 
+            deriving (Eq, Ord, Show, Typeable, Generic)
+instance Binary Update
